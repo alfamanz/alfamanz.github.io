7a0ee8c0cfe13050ce1036f50666f9dd96ea3427
alfamanz <alfarisy.9920@gmail.com>
2024-08-16 21:45:08 +0700

    Add files via upload

---
+++ socks.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+#===========WARNA TEKS============#
+err="\033[31m[ERROR]\033[0m"
+putih='\e[0m'
+hitam="\033[30m"
+merah="\033[31m"
+hijau="\033[32m"
+kuning="\033[33m"
+biru="\033[34m"
+ungu="\033[35m"
+cyan="\033[36m"
+putih="\033[37m"
+sks="[ ${hijau}OK ${putih}]"
+ip=$(curl ifconfig.me)
+#=====================================
+clear
+echo -e "$hijau ____             _        ____    ___           _        _ _           ";
+echo -e "/ ___|  ___   ___| | _____| ___|  |_ _|_ __  ___| |_ __ _| | | ___ _ __ ";
+echo -e "\___ \ / _ \ / __| |/ / __|___ \   | || '_ \/ __| __/ _\` | | |/ _ \ '__|";
+echo -e " ___) | (_) | (__|   <\__ \___) |  | || | | \__ \ || (_| | | |  __/ |   ";
+echo -e "|____/ \___/ \___|_|\_\___/____/  |___|_| |_|___/\__\__,_|_|_|\___|_|   ";
+echo -e "\e[0m"
+echo ""
+echo ""
+echo -e "${kuning}Sc By Alfa${putih}"
+echo -e "${kuning}Wa : 087715768324${putih}"
+echo -e "${kuning}Tele : @faxsenpai${putih}"
+echo ""
+echo ""
+read -p "$(echo -e "${cyan}Buat User Baru Untuk Socks5 : $putih")" usernya
+echo ""
+read -p "$(echo -e "${biru}Klik Enter Untuk Melanjutkan Instalasi : > $putih")"
+sudo apt update
+sudo apt install -y dante-server net-tools ufw
+sudo rm /etc/danted.conf
+echo "logoutput: syslog
+user.privileged: root
+user.unprivileged: nobody
+
+# The listening network interface or address.
+internal: 0.0.0.0 port=1080
+
+# The proxying network interface or address.
+external: ens3
+
+# socks-rules determine what is proxied through the external interface.
+socksmethod: username
+
+# client-rules determine who can connect to the internal interface.
+clientmethod: none
+
+client pass {
+    from: $ip/0 to: 0.0.0.0/0
+}
+
+socks pass {
+    from: 0.0.0.0/0 to: 0.0.0.0/0
+}" > danted.conf
+sudo mv danted.conf /etc/danted.conf
+sudo useradd -r -s /bin/false $usernya
+echo -e "${ungu}Buat password untuk socks5$putih"
+sudo passwd $usernya
+sudo systemctl restart danted
+echo -e "$sks Install Socks5 Telah Berhasil"
+sleep 2
+clear
+echo -e "Socks5 Telah Siap digunakan : ${hijau}$ip:1080$putih"

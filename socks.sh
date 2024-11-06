5d189f4b02adc699a862754c41ea91c372c0bfb3
alfamanz <alfarisy.9920@gmail.com>
2024-11-06 18:51:53 +0700

    Update socks.sh

--- socks.sh
+++ socks.sh
@@ -60,9 +60,7 @@ sudo useradd -r -s /bin/false $usernya
 echo -e "${ungu}Buat password untuk socks5$putih"
 sudo passwd $usernya
 sudo iptables -A INPUT -p tcp --dport 1080 -j ACCEPT
-sudo ufw enable
 sudo ufw allow 1080/tcp
-sudo ufw allow 22
 sudo systemctl restart danted
 echo -e "$sks Install Socks5 Telah Berhasil"
 sleep 2

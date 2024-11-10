22a9c6c5bcc66bb092fd10d0a18d2f391d5598f7
alfamanz <alfarisy.9920@gmail.com>
2024-11-10 18:29:10 +0700

    Add files via upload

---
+++ ch.sh
@@ -0,0 +1,28 @@
+#
+ip=(curl ifconfig.me)
+
+echo "logoutput: syslog
+user.privileged: root
+user.unprivileged: nobody
+
+# The listening network interface or address.
+internal: 0.0.0.0 port=1080
+
+# The proxying network interface or address.
+external: ens3
+
+# socks-rules determine what is proxied through the external interface.
+socksmethod: username
+
+# client-rules determine who can connect to the internal interface.
+clientmethod: none
+
+client pass {
+    from: $ip/0 to: 0.0.0.0/0
+}
+
+socks pass {
+    from: 0.0.0.0/0 to: 0.0.0.0/0
+}" > danted.conf
+sudo mv danted.conf /etc/danted.conf
+echo "SUKSES CHANGE IP"

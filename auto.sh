5133c030b134afbe4d1bc698b25fa91720eed152
alfamanz <alfarisy.9920@gmail.com>
2024-12-25 02:04:53 +0700

    Add files via upload

--- auto.sh
+++ auto.sh
@@ -1,6 +1,7 @@
-NPC=$(nproc)
 wget https://github.com/BinaryExpr/spectre-miner/releases/download/v0.6.26/spectre_miner_x64-v0.6.26_linux.tar.gz
 tar -xvf spectre_miner_x64-v0.6.26_linux.tar.gz
 cd spectre_miner_x64
-screen -d -m -S alfa ./spectre-miner -d eu.spectre-network.nevermine.io:55555 -w spectre:qzy0snxnkdss2k79kmr3ncp6haq5wefqtuljhhjlqhvfdkmr7e9hcv2r4xejq -t $NPC
+echo "NPC=$(nproc)
+./spectre-miner -d eu.spectre-network.nevermine.io:55555 -w spectre:qzy0snxnkdss2k79kmr3ncp6haq5wefqtuljhhjlqhvfdkmr7e9hcv2r4xejq -t $NPC" > mine.sh
+screen -d -m -S alfa bash mine.sh
 echo "Berhasil mining"
